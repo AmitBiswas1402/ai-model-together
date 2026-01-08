1e994a7a14c9ea0d0264abdc191008f1d34919f3
AmitBiswas1402 <amit.142biswas@gmail.com>
2026-01-08 15:34:02 +0530

    DB and schema

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,14 +1,40 @@
 "use client"
 
+import { Button } from "@/components/ui/button";
 import Image from "next/image"
+
+const MenuOptions = [
+  {
+    name: "Pricing",
+    path: "/pricing",
+  },
+  {
+    name: "Contact us",
+    path: "/contact",
+  },
+];
 
 const Header = () => {
   return (
     <div className="flex items-center justify-between p-3 shadow">
-        {/* logo */}
+      {/* logo */}
       <div className="flex gap-2 items-center">
-        <Image src={"/globe.svg"} alt="logo" width={35} height={35} />
-        <h2 className="font-bold text-xl">AI Creator</h2>
+        <Image src={"/favicon.ico"} alt="logo" width={35} height={35} />
+        <h2 className="font-bold text-xl">Web Creator</h2>
+      </div>
+
+      <div className="flex gap-3">
+        {MenuOptions.map((menu, index) => (
+          <Button key={index} variant={"ghost"}>
+            {menu.name}
+          </Button>
+        ))}
+      </div>
+
+      <div>
+        <Button>
+          Get Started 
+        </Button>
       </div>
 
     </div>
